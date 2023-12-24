b30cc9766c83c866c93e89c51ab9fe583cd46b39
12354was <107169221+12354was@users.noreply.github.com>
2023-12-24 19:36:31 +0700

    Delete lab8 directory

--- lab8/app/src/main/java/com/example/lab8/Equation.kt
+++
@@ -1,6 +0,0 @@
-package com.example.lab8
-
-class Equation(stringValue: String, answer: Int) {
-    var stringValue = stringValue
-    var answer = answer
-}
--- lab8/app/src/main/java/com/example/lab8/OnDataListener.kt
+++
@@ -1,5 +0,0 @@
-package com.example.lab8
-
-interface OnDataListener {
-    fun onData(Data: Int)
-}
